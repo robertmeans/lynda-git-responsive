bbe5c7ee976bdabbbed6ff787b6c570d5191a76d
robertmeans <robert@robertmeans.com>
2015-03-27 13:45:43 -0600

    initial commit - for real this time

---
+++ responsive/builds/production/the-other-stuff.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Landon Hotel</title>
+  <link rel="stylesheet" href="css/style.css">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+  <header id="intro">
+    <article class="fullheight">
+      <div class="hgroup">
+        <h1>Landon Hotels</h1>
+        <h2>West London</h2>
+        <p><a href="#welcome"><img src="images/misc/arrow.png" alt="down arrow"></a></p>
+      </div>
+    </article>
+
+    <nav id="nav">
+      <div class="navbar">
+        <div class="brand"><a href="#welcome">Landon <span>Hotel</span></a></div>
+        <ul>
+          <li><a class="icon info" href="#hotelinfo"><span>info</span></a></li>
+          <li><a class="icon rooms" href="#rooms"><span>rooms</span></a></li>
+          <li><a class="icon dining" href="#dining"><span>dining</span></a></li>
+          <li><a class="icon events" href="#events"><span>events</span></a></li>
+          <li><a class="icon attractions" href="#attractions"><span>attractions</span></a></li>
+        </ul>
+      </div><!-- navbar -->
+    </nav>
+  </header>
+<main id="wrapper">
+  <div class="scene" id="welcome">
+    <article class="content">
+      <div class="gallery">
+        <img src="images/hotel/intro_room.jpg" alt="Intro Gallery Room Sample Pictures">
+        <img src="images/hotel/intro_pool.jpg" alt="Intro Gallery Pool Sample Pictures">
+        <img src="images/hotel/intro_dining.jpg" alt="Intro Gallery Dining Sample Pictures">
